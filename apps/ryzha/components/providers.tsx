a6827eb2b31005a46c0696112f1f3f3257dda171
sushmit.ghosh <sushmit.ghosh@blocknox.de>
2026-05-28 14:09:13 +0200

    fix: add SessionProvider to Providers - fixes useSession undefined error in dashboard

--- apps/ryzha/components/providers.tsx
+++ apps/ryzha/components/providers.tsx
@@ -1,18 +1,21 @@
 "use client"
 import { ThemeProvider } from "./theme-provider"
 import { Toaster } from "sonner"
+import AuthProvider from "./auth-provider"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="theme-amethyst"
-      enableSystem
-      disableTransitionOnChange
-      themes={["light", "dark", "system", "theme-amethyst", "theme-forge", "theme-vertex", "theme-quantum", "theme-void", "theme-oasis", "theme-azure", "theme-dune", "theme-blush"]}
-    >
-      {children}
-      <Toaster position="top-right" richColors />
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="theme-amethyst"
+        enableSystem
+        disableTransitionOnChange
+        themes={["light", "dark", "system", "theme-amethyst", "theme-forge", "theme-vertex", "theme-quantum", "theme-void", "theme-oasis", "theme-azure", "theme-dune", "theme-blush"]}
+      >
+        {children}
+        <Toaster position="top-right" richColors />
+      </ThemeProvider>
+    </AuthProvider>
   )
 }
