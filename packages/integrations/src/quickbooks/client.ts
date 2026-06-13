d0107380456e6a8a8e01dc74dd93fc0efc22c4b2
sushmit.ghosh <sushmit.ghosh@blocknox.de>
2026-06-13 08:46:14 +0200

    fix: auto-detect QB sandbox vs production from token x.org header

--- packages/integrations/src/quickbooks/client.ts
+++ packages/integrations/src/quickbooks/client.ts
@@ -1,10 +1,21 @@
 import axios, { AxiosInstance } from "axios"
 
-const QB_API_BASE = "https://quickbooks.api.intuit.com/v3/company"
+const QB_API_PROD    = "https://quickbooks.api.intuit.com/v3/company"
+const QB_API_SANDBOX = "https://sandbox-quickbooks.api.intuit.com/v3/company"
+
+function isSandboxToken(accessToken: string): boolean {
+  try {
+    const header = JSON.parse(Buffer.from(accessToken.split(".")[0], "base64url").toString())
+    return header["x.org"] === "H0"
+  } catch {
+    return false
+  }
+}
 
 export function createQBClient(accessToken: string, realmId: string): AxiosInstance {
+  const base = isSandboxToken(accessToken) ? QB_API_SANDBOX : QB_API_PROD
   return axios.create({
-    baseURL: `${QB_API_BASE}/${realmId}`,
+    baseURL: `${base}/${realmId}`,
     headers: {
       Authorization: `Bearer ${accessToken}`,
       Accept: "application/json",
